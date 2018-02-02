219c439d16e435acf0c99ee382bd4955ee8c143d
knightza123 <knightza123@hotmail.com>
2018-02-02 14:44:41 +0700

    fix label to name and for mobile

--- controller/Application.js
+++ controller/Application.js
@@ -6,7 +6,7 @@ const config = require('../config');
 
 exports.getactivities = function (req,res,next) {
         models.Activities.findAll({
-            attributes:['id',['name','label']]
+            attributes:['id','name']
         }).then(activities=>{
             res.send(JSON.stringify(activities));
         })
