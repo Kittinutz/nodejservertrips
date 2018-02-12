8b23dcc8d1115f09263e5df2de7c15b06b5409a7
knightza123 <knightza123@hotmail.com>
2018-02-12 18:24:14 +0700

    show trip by id

--- controller/TripController.js
+++ controller/TripController.js
@@ -3,19 +3,19 @@ const config = require('../config');
 const jwt = require('jwt-simple');
 
 
-
 function tokenForUser(user) {
     const timestamp = new Date().getTime();
     return jwt.encode({sub: user.id, iat: timestamp}, config.secret);
 
 }
-function tokenDecode(user){
-    return jwt.decode(user,config.secret);
+
+function tokenDecode(user) {
+    return jwt.decode(user, config.secret);
 }
 
-exports.createtrip = function(req,res,next) {
+exports.createtrip = function (req, res, next) {
 
-     var user = tokenDecode(req.headers.authorization);
+    var user = tokenDecode(req.headers.authorization);
     models.Trip.create({
         name: req.body.name,
         description: req.body.description,
@@ -23,28 +23,49 @@ exports.createtrip = function(req,res,next) {
         location: req.body.first.DestinationSelect.value,
         price: req.body.price,
         schedules: req.body.schedule
-    },{
-        include:[models.Schedule]
-    }).then(response=>{
-       response.setPlaces(req.body.first.placeselect);
-       res.send(response);
+    }, {
+        include: [models.Schedule]
+    }).then(response => {
+        response.setPlaces(req.body.first.placeselect);
+        res.send(response);
     });
 
 }
-exports.show =function(req,res,next){
+exports.show = function (req, res, next) {
     var user = tokenDecode(req.headers.authorization);
     models.Trip.findAll(
-        {where:
+        {
+            where:
                 {
-                    creater_id:user.sub
+                    creater_id: user.sub
                 },
 
-        include:[{
-        model:models.Schedule,
-        as:'schedules'
-    }]
+            include: [{
+                model: models.Schedule,
+                as: 'schedules'
+            }]
         }
-        ).then(respone=>{
+    ).then(respone => {
         res.send(respone);
     });
+}
+
+exports.showById = function (req, res, next) {
+
+    var id = req.params.id;
+    models.Trip.findAll(
+        {
+            where:
+                {
+                    id: id
+                },
+            include: [{
+                model: models.Places
+            }, {
+                model: models.Schedule
+            }]
+        }
+    ).then(response=>{
+        res.send(response)
+    })
 }
