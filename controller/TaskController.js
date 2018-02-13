45b3b7e551ec739eee51037eac4c4306ae4746f0
knightza123 <knightza123@hotmail.com>
2018-02-13 22:15:24 +0700

    socket + edit image in Trip

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,8 +1,11 @@
 const models = require('../model/Providers');
-
+const socket = require('../index');
 // const Op = config.Sequelize.Op;
-const config = require('../config');
+const Se = require('../config/sequelize');
 const jwt = require('jwt-simple');
+const config = require('../config');
+
+
 
 
 function tokenForUser(user) {
@@ -14,10 +17,23 @@ function tokenForUser(user) {
 function tokenDecode(user) {
     return jwt.decode(user, config.secret);
 }
+function getnewt(value){
 
+    socket.io.on('connection',(client)=>{
+        client.emit('news', value);
+        client.on('my other event', function (data) {
+            if(data.my=='data'){
+                client.emit('news',{hell:'yeah'});
+            }
+            if(data.my==''){
+                client.emit('news',{hell:'ohyeah'});
+            }
+        });
+    });
+}
 exports.CreateTask = function (req, res, next) {
-
     var user = tokenDecode(req.headers.authorization);
+    getnewt(req.body.languages);
     models.Task.create({
 
         user_id: user.sub,
@@ -32,22 +48,6 @@ exports.CreateTask = function (req, res, next) {
 
 };
 
-exports.getTask = function (req, res, next) {
-
-    models.Task.findAll({
-        include: [{
-            model: models.Languages,
-            where: {id: 22}
-        },
-            {
-                model: models.User
-            }
-        ]
-    }).then(response => {
-        res.send(response);
-    })
-
-};
 
 exports.getOwnTask = function (req, res, next) {
     console.log(req.headers.authorization);
@@ -70,17 +70,29 @@ exports.getOwnTask = function (req, res, next) {
     })
 };
 exports.Notification = (req, res, next) => {
-    var guide = tokenDecode(req.headers.authorization);
-    console.log(req.headers.authorization);
+    var user = tokenDecode(req.headers.authorization);
+
+
     models.Task.findAll({
         include: [
             {
-                model:models.Languages,
-                required: true,
-                include: [{model:models.Guide ,where:{id:guide.sub},attributes:[]}]
+                model: models.User
+            },
+            {
+                model: models.Languages,
+                where: {id: Se.Sequelize.col('tasks.id')},
+                attributes:[['id','value'],['languages','label']],
+                through: {attributes: []},
+                include: [{
+                    model: models.Guide,
+                    through: {attributes: []},
+                    where: {id: user.sub},
+                    attributes:[]
+                }]
             }
         ]
     }).then(response => {
         res.send(response);
     });
 };
+
